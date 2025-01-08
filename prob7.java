5c33eea29cbc18eb200db6008d5c2651c620d306
heynexus007 <abthegamer007@gmail.com>
2025-01-08 11:43:43 +0530

    Perimeter of a Rectangle

---
+++ prob7.java
@@ -0,0 +1,13 @@
+import java.util.*;
+//Perimeter of a Rectangle
+public class prob4 {
+    public static void main(String[] args) {
+        Scanner ip=new Scanner(System.in);
+        double length=ip.nextDouble();
+        double width=ip.nextDouble();
+		
+		double Perimeter= (P*R*T)/100;
+ 
+        System.out.println(Perimeter);
+    }
+}
